c0ee748a00da9cd3a10fb0a8e7e9896068257ca2
Ethan Do <trilv8@gmail.com>
2025-02-22 22:24:03 -0500

    Speech_to_text

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import './navbar.css'
+import {GetSpeech} from '../../speech_convert/speech_text';
+import {useHover} from '../../speech_convert/hover_auto_clicking';
 
 
 const Navbar = () => {
+    const handleMicClick = () => {
+        console.log("Mic button clicked automatically!");  
+        GetSpeech(); 
+    };
+    const [hoverRef, isHovering] = useHover(handleMicClick, 2000);
     return (
         <nav className="nav-menu">
             <div>
@@ -10,6 +17,28 @@ const Navbar = () => {
             <div>
                 <button>Home</button>
             </div>
+            <div>
+                <button 
+                    // onClick={handleMicClick}
+                    ref={hoverRef}
+                    style={{
+                        backgroundColor: isHovering ? "lightblue" : "",
+                        border: isHovering ? "2px solid black" : "1px solid gray"
+                    }}
+                >
+                    Mic
+                </button>
+            </div>
+            {/* <div
+                ref={hoverRef}
+                style={{
+                    backgroundColor: isHovering ? 'lightblue' : 'white',
+                    padding: '20px',
+                    border: '1px solid black',
+                }}
+                >
+                {isHovering ? 'Hovering!' : 'Not hovering.'}
+            </div> */}
         </nav>
     );
   };
